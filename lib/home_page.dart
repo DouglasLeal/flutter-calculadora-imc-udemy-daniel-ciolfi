8a129637f229d49f45c87526563e0f08bd902d82
Douglas Leal <douglas.leal@live.com>
2023-02-04 12:20:55 -0300

    Adicionado ícone e TextFields

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -22,6 +22,34 @@ class _HomePageState extends State<HomePage> {
           )
         ],
       ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: const [
+          Icon(
+            Icons.person_outline,
+            size: 120,
+            color: Colors.green,
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Peso (Kg)",
+              labelStyle: TextStyle(color: Colors.green),
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green, fontSize: 24),
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Altura (cm)",
+              labelStyle: TextStyle(color: Colors.green),
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green, fontSize: 24),
+          ),
+        ],
+      ),
     );
   }
 }
